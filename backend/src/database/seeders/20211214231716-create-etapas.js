2a79aaebea9387b1b78b6be9e66ac9d5a0306324
marco <marco_subero1996@hotmail.com>
2021-12-27 10:30:49 -0400

    defaul create at seeder

--- backend/src/database/seeders/20211214231716-create-etapas.js
+++ backend/src/database/seeders/20211214231716-create-etapas.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { date } = require("faker/locale/zh_TW");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
@@ -9,56 +11,78 @@ module.exports = {
           nombre: "Prospección",
           tipo: "oportunidad",
           paso: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           nombre: "Calificación",
           tipo: "oportunidad",
           paso: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           nombre: "Análisis de Necesidades",
           tipo: "oportunidad",
           paso: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           nombre: "Análisis de Necesidades",
           tipo: "oportunidad",
           paso: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           nombre: "Propuesta",
           tipo: "oportunidad",
           paso: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           nombre: "Toma de decisiones",
           tipo: "oportunidad",
           paso: 6,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           nombre: "Análisis de percepción",
           tipo: "oportunidad",
           paso: 7,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           nombre: "Propuesta",
           tipo: "oportunidad",
           paso: 8,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           nombre: "Negociación",
           tipo: "oportunidad",
           paso: 9,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           nombre: "Cerrada - Ganada",
           tipo: "oportunidad",
           paso: 10,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           nombre: "Cerrada - Perdida",
           tipo: "oportunidad",
           paso: 10,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
       ],
       {}
